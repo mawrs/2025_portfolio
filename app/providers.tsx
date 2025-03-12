1868c099fc46ac1c7ca1b26aadf08fd3f3319f30
Martin <tejedam21@gmail.com>
2025-03-11 21:02:58 -0700

    Fix PostHog initialization for server-side rendering

--- app/providers.tsx
+++ app/providers.tsx
@@ -13,23 +13,21 @@ declare global {
   }
 }
 
-// PostHog initialization script
-if (typeof window !== 'undefined') {
-  // @ts-ignore - PostHog's type definition doesn't match their documentation
-  !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-  
-  // Initialize PostHog
-  window.posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-    loaded: function(posthog: typeof window.posthog) {
-      if (process.env.NODE_ENV === 'development') posthog.debug()
-    }
-  });
-}
-
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Initialize PostHog only on the client side
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        loaded: function(posthog) {
+          if (process.env.NODE_ENV === 'development') posthog.debug()
+        }
+      })
+    }
+  }, [])
+
   return (
-    <PHProvider client={window.posthog}>
+    <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
